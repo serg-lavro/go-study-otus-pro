a99d23694659c417c61ed6b063b2263f931fe62a
Sergei Lavrentev <sergeylavrentyev8@gmail.com>
2025-08-28 17:35:41 +0200

    [hw2] Implement string unpacking
    Signed-off-by: Sergei Lavrentev <sergeylavrentyev8@gmail.com>

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,50 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inputStr string) (string, error) {
+	if inputStr == "" {
+		return "", nil
+	}
+
+	rn, runeSz := utf8.DecodeRuneInString(inputStr)
+	if unicode.IsDigit(rn) {
+		return "", ErrInvalidString
+	}
+
+	var outputStr strings.Builder
+	inputStr = inputStr[runeSz:]
+	outputStr.WriteString(string(rn))
+
+	prevRn := rn
+	for _, curRn := range inputStr {
+		if unicode.IsDigit(prevRn) && unicode.IsDigit(curRn) {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(curRn) {
+			cnt, _ := strconv.Atoi(string(curRn))
+			if cnt > 0 {
+				outputStr.WriteString(strings.Repeat(string(prevRn), cnt-1))
+			} else {
+				suf := string(prevRn)
+				tmpStr := outputStr.String()
+				tmpStr = strings.TrimSuffix(tmpStr, suf)
+				outputStr.Reset()
+				outputStr.WriteString(tmpStr)
+			}
+		} else {
+			outputStr.WriteString(string(curRn))
+		}
+		prevRn = curRn
+	}
+	res := outputStr.String()
+	return res, nil
 }
