d277e428d23b535814c744a6ccdab5b657f67d7f
Sergei Lavrentev <sergeylavrentyev8@gmail.com>
2025-09-14 07:12:46 +0200

    [hw03] Complete the task
    Signed-off-by: Sergei Lavrentev <sergeylavrentyev8@gmail.com>

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type keyVal struct {
+	word  string
+	count int
+}
+
+func Top10(inputText string) []string {
+	words := strings.Fields(inputText)
+
+	wordCnt := make(map[string]int)
+	for _, w := range words {
+		wordCnt[w]++
+	}
+
+	counts := make([]keyVal, 0, len(wordCnt))
+	for w, c := range wordCnt {
+		counts = append(counts, keyVal{w, c})
+	}
+
+	sort.Slice(counts, func(i, j int) bool {
+		if counts[i].count == counts[j].count {
+			return counts[i].word < counts[j].word
+		}
+		return counts[i].count > counts[j].count
+	})
+
+	outputLen := min(10, len(counts))
+
+	output := make([]string, outputLen)
+	for i := 0; i < outputLen; i++ {
+		output[i] = counts[i].word
+	}
+
+	return output
 }
