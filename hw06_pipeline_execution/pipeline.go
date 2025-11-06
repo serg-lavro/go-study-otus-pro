263eb77b4d50dd1f2590076ab6760461256f7e4f
Sergei Lavrentev <sergeylavrentyev8@gmail.com>
2025-11-06 11:15:02 +0100

    [hw06] Complete the task
    Signed-off-by: Sergei Lavrentev <sergeylavrentyev8@gmail.com>

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "sync/atomic"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +11,69 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var canceled atomic.Bool
+	interimIn := make(Bi)
+
+	// goroutine for managing input: receiving and closing
+	go func() {
+		defer close(interimIn)
+		for {
+			i, ok := <-in
+			if !ok || canceled.Load() {
+				return
+			}
+			interimIn <- i
+		}
+	}()
+
+	out := Out(interimIn)
+	for _, st := range stages {
+		out = st(out)
+	}
+
+	// goroutine for collecting output and managing shutdown
+	buf := []interface{}{}
+	finishPipeline := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-done:
+				canceled.Store(true)
+				// flushing output so the close of input would propagate through
+				// upstream stages; closing finish channel prior flush for perf
+				close(finishPipeline)
+				for {
+					_, ok := <-out
+					if !ok {
+						break
+					}
+				}
+				return
+			case v, ok := <-out:
+				if !ok {
+					close(finishPipeline)
+					return
+				}
+				buf = append(buf, v)
+			}
+		}
+	}()
+
+	res := make(Bi)
+
+	<-finishPipeline
+
+	if canceled.Load() {
+		close(res)
+		return res
+	}
+
+	go func() {
+		defer close(res)
+		for _, v := range buf {
+			res <- v
+		}
+	}()
+
+	return res
 }
